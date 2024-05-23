d56fb426ef8bde293f725462389eefbbb5efe3b8
DIEGO CUBILLOS <dcubillos@awake.travel>
2024-05-23 11:11:26 -0500

    homework uno js

---
+++ archivo-ejemplo.js
@@ -0,0 +1,9 @@
+// variables :  son contenedores de elementos que podemos usar despues
+// string : son un tipo de dato relacionado al texto
+// funciones :  son herramientas que nos ayudan a automatizar una tarea
+// argumentos : son los parametros que recibe la funcion
+// return :  es lo que nos ayuda a ingresar al resultado de la funcion corta la ejecucion y muestra el resultado
+// valores booleanos: son con un interruptor que prende y apaga es que da el ingreso o lo deniega
+
+
+
